d6dba1e132d2c9af7ae7ae1792ee5893fb69e5ad
Nikita Antonov <nikita45454@gmail.com>
2023-03-30 21:15:46 +0500

    fix: пропушены кавычки

--- sql/create-db.js
+++ sql/create-db.js
@@ -1,3 +1,4 @@
+import '#app/config/index.js';
 import { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } from '#app/config/app.js';
 import mariadb from 'mariadb';
 
@@ -8,7 +9,7 @@ const connection = mariadb.createConnection({
 });
 
 connection.then(db => {
-  db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`)
+  db.query(`CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\``)
   .then(console.log)
   .catch(console.log)
   .finally(() => db.end());
